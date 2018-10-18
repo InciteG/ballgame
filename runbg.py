24f9a550da4747a162efc859bedc50badd04780f
Gary Guo <43452949+InciteG@users.noreply.github.com>
2018-10-18 16:12:10 -0400

    Add files via upload
    Moved functions to bgfunctions.py from bgdatacol.
    Changed how data is stored - moved away from OOP, made data storage less specific so others can use.
    New functions - defaultget and scrape.
    url_set is unchanged
    runbg.py is updated to run through bgfunctions for years 2016-17, 2017-18, 2018-19

--- runbg.py
+++ runbg.py
@@ -1,20 +1,22 @@
 # BallGame Start
 #run bgdatacol, run ballgameupdater
-import bgdatacol
-import sqlite3
 
-conn = sqlite3.connect('Test1.db')
+import sqlite3
+import bgfunctions
+
+conn = sqlite3.connect('Test3.db')
 cur = conn.cursor()
 
+"""Check if url table exists and runs bg.functions.url_set() if not"""
 cur.execute("SELECT name FROM sqlite_master;")
 a = cur.fetchall()
 conn.commit()
 b = []
-for item in a:
+for item in a: #get list of table names in database to check for 'NBA URL Table'
     item2 = ''.join(item)
     b.append(item2)
 
-if 'NBA URL Table' in b:
+if 'NBA URL Table' in b: #if NBA URL table exists, withdraw team and respective url to python variables
     teamurltag = []
     teamnames = []
     cur.execute('SELECT Url FROM[NBA URL Table];')
@@ -29,7 +31,7 @@ if 'NBA URL Table' in b:
         str = "".join(team)
         teamnames.append(str)
 else:
-     bgdatacol.url_set()
+     bgfunctions.url_set()
      teamurltag = []
      teamnames = []
      cur.execute('SELECT Url FROM [NBA URL Table];')
@@ -44,7 +46,8 @@ else:
          str = "".join(team)
          teamnames.append(str)
 
-for teamurltag, teamnames in zip(teamurltag, teamnames):
-    bgdatacol.NBAScrape(teamurltag, teamnames)
-    # bgdatacol.NBAUpdate(teamurltag, teamnames)
-bgdatacol.driver.quit()
+for teams, url in zip(teamnames, teamurltag):
+    years = ['2016-17', '2017-18', '2018-19']
+    for x in years:
+        bgfunctions.defaultget(teams, url, x)
+bgfunctions.driver.close()
