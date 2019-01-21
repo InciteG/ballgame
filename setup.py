2578595cf4b2770c446b6c78224caad32b4ab464
Gary Guo <43452949+InciteG@users.noreply.github.com>
2019-01-21 18:07:25 -0500

    Add files via upload
    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ballgame',
+    version='0.1',
+    packages=['ballgame',],
+    license='GNU General Public License',
+    long_description=open('README.txt').read(),
+)
